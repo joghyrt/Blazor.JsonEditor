5df552b5edfd69797e03cd64bd18a766a061a792
Ivan Akimov <iakimov@knowyourcustomer.com>
2023-10-14 12:29:45 +0200

    Demo wasm web app added

---
+++ Blazor.JsonEditor.Demo.Wasm/Model/IndexModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Blazor.JsonEditor.Demo.Wasm.Model
+{
+    public class IndexModel
+    {
+        public string Json { get; set; }
+    }
+}
---
+++ Blazor.JsonEditor.Demo.Wasm/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿using Blazor.JsonEditor.Demo.Wasm.Model;
+
+namespace Blazor.JsonEditor.Demo.Wasm.Pages
+{
+    public partial class Index
+    {
+        private IndexModel DemoJson { get; set; } = new()
+        {
+            Json =
+                "{\"Nullable\": null, \"String\": \"random\", \"Number\": 1, \"Array\": [1,2,3], \"True\": true, \"False\": false, \"Object\": { \"Nullable\": null, \"String\": \"random\", \"Number\": 1, \"Array\": [1,2,3], \"True\": true, \"False\": false } }"
+        };
+    }
+}
